a9123e1df4ea68eafb5c68f9b1961370c487b98d
mostafa <mostafahr@hotmail.com>
2018-05-17 18:38:49 -0700

    CS 3520 Ex 5

---
+++ controller/database.js
@@ -0,0 +1,40 @@
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://mostaradwan:Uae200noo!@ds011860.mlab.com:11860/heroku_xclcfh02';
+
+
+/** getAllRoutes controller logic that current does model logic too -connects to Mongo database and
+ * queries the Routes collection to retrieve all the routes and build the output usig the
+ * ejs template mongodb.ejs found in views directory
+ * @param request
+ * @param response
+ *
+ */
+module.exports.getAllOrders =  function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+        if(err) throw err;
+
+
+        //get handle to the databse
+        var theDatabase = client.db('heroku_xclcfh02');
+
+
+        //get collection of routes
+        var Routes = theDatabase.collection('ORDERS');
+
+
+        //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
+        //   array called docs that you  forward to the  getAllRoutes.ejs view for use there
+        Routes.find().toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('getAllOrders', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        client.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+};//end function
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RMS' });
-});
+//LOAD the various controllers
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://Rfinazzo4:Rio-fizzo4@ds225840.mlab.com:25840/heroku_hnqdq9m4';
+
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.post('/getAllOrders', function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+        if(err) throw err;
+        //get handle to the database
+        var theDatabase = client.db('heroku_hnqdq9m4');
+
+        router.get('/getAllOrders', function(req, res, next)
+        {
+
+            res.render('index', { title: 'Express' });
+        });
+
+
+        //get collection of Routes
+        var Routes = theDatabase.collection('Routes');
+        //get all Routes
+        Routes.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('mongodb', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        client.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+
+});//end XXX.get
+
+//CODE to route /getAllRoutes to appropriate  Controller function
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection w
+//      and Render information iwith an ejs view
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
 module.exports = router;
-
-// whats up
-
-//now processing post
-router.post('/storeData', function(req, res, next) {
-//expecting data variable called order--retrieve value using body-parser
-var value_name = req.body.order  //retrieve the data associated with order
-res.send("order succesfully received: " + value_name);
-});
